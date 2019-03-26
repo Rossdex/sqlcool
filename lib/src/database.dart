f78607ea351cb25fee7b03fd13a0a86083c38326
Ross Dexter <ross@senses.co.uk>
2019-03-26 11:04:22 +0000

    Added a transaction function to the database class which takes a transaction and will fireoff a database change event when complete

--- lib/src/database.dart
+++ lib/src/database.dart
@@ -19,7 +19,7 @@ class Db {
   final _mutex = new Lock();
   final Completer<Null> _readyCompleter = Completer<Null>();
   final StreamController<DatabaseChangeEvent> _changeFeedController =
-  StreamController<DatabaseChangeEvent>.broadcast();
+      StreamController<DatabaseChangeEvent>.broadcast();
   File _dbFile;
   bool _isReady = false;
 
@@ -50,14 +50,81 @@ class Db {
   /// The database can be initialized either from an asset file
   /// with the [fromAsset] parameter or from some create table queries
   /// with the [queries] parameter.
-  Future<void> init({
-    Database onOpen,
-    String path,
-    bool absolutePath = false,
-    List<String> queries = const <String>[],
-    bool verbose = false,
-    String fromAsset = "",
-    bool debug = false}) async {
+  Future<void> init(
+      {Database database,
+      String path,
+      bool absolutePath = false,
+      List<String> queries = const <String>[],
+      bool verbose = false,
+      String fromAsset = "",
+      bool debug = false}) async {
+    Database sqlDatabase;
+    String dbpath;
+    if (debug) Sqflite.setDebugModeOn(true);
+    if (path != null) {
+      dbpath = path;
+      if (!absolutePath) {
+        Directory documentsDirectory = await getApplicationDocumentsDirectory();
+        dbpath = documentsDirectory.path + "/" + path;
+      }
+      if (verbose) {
+        print("INITIALIZING DATABASE at " + dbpath);
+      }
+      // copy the database from an asset if necessary
+      if (fromAsset != "") {
+        File file = File(dbpath);
+        if (!file.existsSync()) {
+          if (verbose) {
+            print("Copying the database from asset $fromAsset");
+          }
+          List<int> bytes;
+          try {
+            // read
+            ByteData data = await rootBundle.load("$fromAsset");
+            bytes =
+                data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+          } catch (e) {
+            throw ("Unable to read database from asset: $e");
+          }
+          try {
+            // create the directories path if necessary
+            if (!file.parent.existsSync()) {
+              file.parent.createSync(recursive: true);
+            }
+            // write
+            await file.writeAsBytes(bytes);
+          } catch (e) {
+            throw ("Unable to write database from asset: $e");
+          }
+        }
+      }
+    }
+
+    if (database != null) {
+      print(database);
+      print("database");
+      sqlDatabase = database;
+    } else if (dbpath != null) {
+      sqlDatabase = await openDatabase(
+        dbpath,
+        version: 1,
+        onCreate: (Database _db, int version) async {
+          if (queries.isNotEmpty) {
+            for (String q in queries) {
+              Stopwatch timer = Stopwatch()..start();
+              await _db.execute(q);
+              if (verbose) {
+                String msg = "$q in ${timer.elapsedMilliseconds} ms";
+                print(msg);
+              }
+            }
+          }
+        },
+      );
+    } else {
+      throw ("No Database or Path found");
+    }
+
     if (this._db == null) {
       await _mutex.synchronized(() async {
         if (this._db == null) {
@@ -65,7 +132,7 @@ class Db {
           if (verbose) {
             print("OPENING database");
           }
-          this._db = onOpen;
+          this._db = sqlDatabase;
         }
       });
     }
@@ -84,8 +151,7 @@ class Db {
     /// [q] the query to execute
     try {
       if (!_isReady) throw DatabaseNotReady();
-      Stopwatch timer = Stopwatch()
-        ..start();
+      Stopwatch timer = Stopwatch()..start();
       final List<Map<String, dynamic>> res = await this._db.rawQuery(q);
       timer.stop();
       if (verbose) {
@@ -101,13 +167,14 @@ class Db {
   }
 
   /// A select query
-  Future<List<Map<String, dynamic>>> select({@required String table,
-    String columns = "*",
-    String where,
-    String orderBy,
-    int limit,
-    int offset,
-    bool verbose = false}) async {
+  Future<List<Map<String, dynamic>>> select(
+      {@required String table,
+      String columns = "*",
+      String where,
+      String orderBy,
+      int limit,
+      int offset,
+      bool verbose = false}) async {
     /// [table] the table to select from
     /// [columns] the columns to return
     /// [where] the sql where clause
@@ -118,8 +185,7 @@ class Db {
     /// returns the selected data
     try {
       if (!_isReady) throw DatabaseNotReady();
-      Stopwatch timer = Stopwatch()
-        ..start();
+      Stopwatch timer = Stopwatch()..start();
       String q = "SELECT $columns FROM $table";
       if (where != null) {
         q += " WHERE $where";
@@ -148,15 +214,16 @@ class Db {
   }
 
   /// A select query with a join
-  Future<List<Map<String, dynamic>>> join({@required String table,
-    @required String joinTable,
-    @required String joinOn,
-    String columns = "*",
-    int offset = 0,
-    int limit = 100,
-    String orderBy,
-    String where,
-    bool verbose}) async {
+  Future<List<Map<String, dynamic>>> join(
+      {@required String table,
+      @required String joinTable,
+      @required String joinOn,
+      String columns = "*",
+      int offset = 0,
+      int limit = 100,
+      String orderBy,
+      String where,
+      bool verbose}) async {
     /// [table] the table to select from
     /// [joinTable] the table to join from
     /// [joinOn] the columns to join
@@ -169,8 +236,7 @@ class Db {
     /// returns the selected data
     try {
       if (!_isReady) throw DatabaseNotReady();
-      Stopwatch timer = Stopwatch()
-        ..start();
+      Stopwatch timer = Stopwatch()..start();
       String q = "SELECT $columns FROM $table";
       q = "$q INNER JOIN $joinTable ON $joinOn";
       if (where != null) {
@@ -200,9 +266,10 @@ class Db {
   }
 
   /// Insert a row in a table
-  Future<int> insert({@required String table,
-    @required Map<String, dynamic> row,
-    bool verbose = false}) async {
+  Future<int> insert(
+      {@required String table,
+      @required Map<String, dynamic> row,
+      bool verbose = false}) async {
     /// [table] the table to insert into. [row] is a map of the data
     /// to insert
     ///
@@ -212,8 +279,7 @@ class Db {
     await _mutex.synchronized(() async {
       try {
         if (!_isReady) throw DatabaseNotReady();
-        Stopwatch timer = Stopwatch()
-          ..start();
+        Stopwatch timer = Stopwatch()..start();
         String fields = "";
         String values = "";
         int n = row.length;
@@ -254,10 +320,11 @@ class Db {
   }
 
   /// Update some datapoints in the database
-  Future<int> update({@required String table,
-    @required Map<String, String> row,
-    @required String where,
-    bool verbose = false}) async {
+  Future<int> update(
+      {@required String table,
+      @required Map<String, String> row,
+      @required String where,
+      bool verbose = false}) async {
     /// [table] is the table to use, [row] is a map of the data to update
     /// and [where] the sql where clause
     ///
@@ -265,8 +332,7 @@ class Db {
     int updated = 0;
     await _mutex.synchronized(() async {
       if (!_isReady) throw DatabaseNotReady();
-      Stopwatch timer = Stopwatch()
-        ..start();
+      Stopwatch timer = Stopwatch()..start();
       try {
         String pairs = "";
         int n = row.length - 1;
@@ -304,9 +370,8 @@ class Db {
   }
 
   /// Delete some datapoints from the database
-  Future<int> delete({@required String table,
-    String where,
-    bool verbose = false}) async {
+  Future<int> delete(
+      {@required String table, String where, bool verbose = false}) async {
     /// [table] is the table to use and [where] the sql where clause
     ///
     /// Returns a future with a count of the deleted rows
@@ -314,10 +379,9 @@ class Db {
     await _mutex.synchronized(() async {
       if (!_isReady) throw DatabaseNotReady();
       try {
-        Stopwatch timer = Stopwatch()
-          ..start();
+        Stopwatch timer = Stopwatch()..start();
         String q = 'DELETE FROM $table';
-        if(where != null) {
+        if (where != null) {
           q += 'WHERE $where';
         }
 
@@ -343,16 +407,16 @@ class Db {
   }
 
   /// Check if a value exists in the table
-  Future<bool> exists({@required String table,
-    @required String where,
-    bool verbose = false}) async {
+  Future<bool> exists(
+      {@required String table,
+      @required String where,
+      bool verbose = false}) async {
     /// [table] is the table to use and [where] the sql where clause
     ///
     /// Returns a future with true if the data exists
     try {
       if (!_isReady) throw DatabaseNotReady();
-      Stopwatch timer = Stopwatch()
-        ..start();
+      Stopwatch timer = Stopwatch()..start();
       String q = 'SELECT COUNT(*) FROM $table WHERE $where';
       int count = Sqflite.firstIntValue(await _db.rawQuery(q));
       timer.stop();
@@ -372,14 +436,14 @@ class Db {
   }
 
   /// count rows in a table
-  Future<int> count({@required String table, String where, bool verbose = false}) async {
+  Future<int> count(
+      {@required String table, String where, bool verbose = false}) async {
     /// [table] is the table to use and [where] the sql where clause
     ///
     /// Returns a future with the count of the rows
     try {
       if (!_isReady) throw DatabaseNotReady();
-      Stopwatch timer = Stopwatch()
-        ..start();
+      Stopwatch timer = Stopwatch()..start();
       String w = "";
       if (where != null) {
         w = " WHERE $where";
@@ -392,6 +456,28 @@ class Db {
         print(msg);
       }
       return c;
+    } on DatabaseNotReady catch (e) {
+      throw ("${e.message}");
+    } catch (e) {
+      throw (e);
+    }
+  }
+
+  Future<String> transaction({@required action, bool verbose = false}) async {
+    try {
+      if (!_isReady) throw DatabaseNotReady();
+      Stopwatch timer = Stopwatch()..start();
+      _db.transaction(action);
+      timer.stop();
+      String msg = "Transaction compelete in ${timer.elapsedMilliseconds} ms";
+      if (verbose) {
+        print(msg);
+      }
+      _changeFeedController.sink.add(DatabaseChangeEvent(
+          type: DatabaseChange.transaction,
+          executionTime: timer.elapsedMicroseconds));
+
+      return msg;
     } on DatabaseNotReady catch (e) {
       throw ("${e.message}");
     } catch (e) {
--- lib/src/models.dart
+++ lib/src/models.dart
@@ -1,50 +1,59 @@
 import 'package:flutter/foundation.dart';
 
 /// Types of database changes
-enum DatabaseChange { insert, update, delete }
+enum DatabaseChange { insert, update, delete, transaction }
 
 /// A database change event. Used by the changefeed
 class DatabaseChangeEvent {
   /// Default database change event
-  DatabaseChangeEvent(
-    {@required this.type,
+  DatabaseChangeEvent({@required this.type,
     @required this.value,
-    @required this.query,
+    this.query,
     @required this.executionTime,
     @required this.table});
 
-/// Type of the change
-DatabaseChange type;
+  /// Type of the change
+  DatabaseChange type;
 
-/// The Table being changed
-String table;
+  /// The Table being changed
+  String table;
 
-/// Change value: number of items affected
-int value;
+  /// Change value: number of items affected
+  int value;
 
-/// The query that made the changes
-String query;
+  /// The query that made the changes
+  String query;
 
-/// The query execution time
-num executionTime;
+  /// The query execution time
+  num executionTime;
 
-/// Human readable format
-@override
-String toString() {
-  String s = "";
-  if (value > 1) {
-    s = "s";
+  /// Human readable format
+  @override
+  String toString() {
+    String s = "";
+    if (value > 1) {
+      s = "s";
+    }
+    String msg = "";
+    if (type == DatabaseChange.delete) {
+      msg += "$value item$s deleted";
+    } else if (type == DatabaseChange.update) {
+      msg += "$value item$s updated";
+    } else if (type == DatabaseChange.insert) {
+      msg += "$value item$s inserted";
+    } else if (type == DatabaseChange.transaction) {
+      msg += "$value transaction jobs completed";
+    } else {
+      msg += "\n$type : $value";
+    }
+
+    if (query != null) {
+      msg += "\n$query in $executionTime ms";
+    } else if (type == DatabaseChange.transaction) {
+      msg += "\nin $executionTime ms";
+    }
+
+
+    return msg;
   }
-  String msg = "";
-  if (type == DatabaseChange.delete) {
-    msg += "$value item$s deleted";
-  } else if (type == DatabaseChange.update) {
-    msg += "$value item$s updated";
-  } else if (type == DatabaseChange.insert) {
-    msg += "$value item$s inserted";
-  }
-  msg += "\n$type : $value";
-  msg += "\n$query in $executionTime ms";
-  return msg;
-}
 }
